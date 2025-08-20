84180f24a39ed7461a57eef7ef754a53375fe251
Evgeny1337 <genya-sozykin@mal.ru>
2025-08-20 21:47:08 +0500

    Reduced the number of queries to the database

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -93,7 +93,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().values('id','firstname','lastname','phonenumber', 'address')
     serializer = OrderSerializer(orders, many=True)
     return render(request, template_name='order_items.html', context={
         'order_items': serializer.data,
