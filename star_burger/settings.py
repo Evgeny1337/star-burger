05a6170d7c59732652bd0c01607babb21908804a
Evgeny1337 <genya-sozykin@mal.ru>
2025-09-20 15:58:13 +0500

    fixed settings

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -7,7 +7,7 @@ import environ
 
 
 env = environ.Env()
-env.read_env()
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
