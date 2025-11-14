2d54412c97fca033488acf6ac38792500358f0b4
Evgeny1337 <genya-sozykin@mal.ru>
2025-11-14 16:16:10 +0500

    clear

--- geocoordinates/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PlaceCoordinates
-
-@admin.register(PlaceCoordinates)
-class PlaceCoordinatesAdmin(admin.ModelAdmin):
-    list_display = ('address', 'lat', 'lon', 'updated_at')
-    list_filter = ('updated_at',)
-    search_fields = ('address',)
-    readonly_fields = ('updated_at',)
